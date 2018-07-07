af978f42ce66f5f4f6e0f6a950bd3bc4441c606f
Rey Marcos Ivan <40122464+ReyMarcosIvan@users.noreply.github.com>
2018-07-07 12:59:54 -0300

    Add files via upload

--- excepciones/1.py
+++ excepciones/1.py
@@ -3,18 +3,18 @@ continuar=True
 while continuar:
 
     try:
-        a = int(raw_input("Primer Numero: "))
-        b = int(raw_input("Segundo Numero: "))
-        print float(a) / b
+        a = float(raw_input("Primer Numero: "))
+        b = float(raw_input("Segundo Numero: "))
+        if(int(a)==a and int(b)==b):
+            print int(a)/int(b)
+        else:
+            print a / b
     except ValueError as e:
-        print "Solo ingrese numeros "
-        continue
+        print "Solo ingrese numeros"
     except ZeroDivisionError as e:
         print "No se puede dividir un numero por 0"
-        continue
     except Exception as e:
         print e
-        continue
     else:
         letra = "a"
         while (letra != "s" and letra != "n"):
---
+++ excepciones/boots.py
@@ -0,0 +1,4 @@
+import random
+preguntas={"Al pan pan y al vino ...":"vino","Cual de estas palabras no van en la serie: \nperro gato loro elefante":"loro"}
+
+print random(0,10)
